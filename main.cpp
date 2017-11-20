36298209d31c7d9c294514e92642b3d356d8d637
Deni Kljucaric <icedropdev@gmail.com>
2017-11-20 19:05:19 +0100

    samo dodano d u dcselobj

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ void GetFont(HWND parent, LOGFONT &lf)
 	cf.lpLogFont = &lf;
 	ChooseFont(&cf);
 }
+
 int MyDialog::IDD(){
 	return IDD_DIALOG;
 }
@@ -33,18 +34,23 @@ void MainWindow::OnPaint(HDC hdc) {
 		return;
 	RECT screen;
 	GetClientRect(*this,&screen);
-	SetMapMode(hdc, MM_ANISOTROPIC);
-	SetViewportExtEx(hdc, screen.right, screen.bottom, NULL);
-	SetWindowExtEx(hdc,9,text.size(),0);
+	int dy = screen.bottom / text.size();
+	int dx = screen.right / 9;
+	//SetMapMode(hdc, MM_ANISOTROPIC);
+	//SetViewportExtEx(hdc, screen.right, screen.bottom, NULL);
+	//SetWindowExtEx(hdc,18,text.size(),0);
+	Font f(lf);
+	DCSelObj d(hdc,f);
 	for (int i = 0; i < text.size(); i++) {
 		for (int j = 0; j < 8; j++) {
-			RECT r = { j, i, (j + 1), (i + 1) };
-			if ((text[i] & (1 << (7 - j)))==0)
+			RECT r = { j*dx, i*dy, (j + 1)*dx, (i + 1)*dy };
+			if ((text[i] & (1 << (7 - j))) == 0)
 				FillRect(hdc, &r, (HBRUSH)GetStockObject(BLACK_BRUSH));
 		}
-		RECT r = { 8 , i, 9 , (i + 1) };
+		RECT r = { 8*dx , i*dy, 9*dx , (i+1)*dy };
 		::DrawText(hdc, &text[i], 1, &r, DT_CENTER | DT_VCENTER | DT_SINGLELINE | DT_END_ELLIPSIS);
 	}
+
 }
 
 void MainWindow::OnCommand(int id) {
--- main.h
+++ main.h
@@ -19,10 +19,31 @@ public:
 	MainWindow(){
 		::ZeroMemory(&lf, sizeof(lf));
 		_tcscpy(lf.lfFaceName, _T("Arial"));
+		lf.lfHeight = -20;
 	}
 protected:
-	
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+};
+
+class Font {
+	HFONT h;
+public:
+	Font(const LOGFONT& lf) {
+		h = ::CreateFontIndirect(&lf);
+	}
+	Font(const TCHAR* name, int height, bool bold = false, bool italic = false, int angle = 0, bool underline = false) : h(NULL) {
+		h = ::CreateFont(height, 0, angle * 10, 0, bold ? FW_BOLD : FW_NORMAL, italic, underline, 0, 0, 0, 0, 0, 0, name);
+	}
+	~Font() { if (h) ::DeleteObject(h); }
+	operator HFONT() { return h; }
+};
+class DCSelObj {
+	HDC hdc;
+	HGDIOBJ hOld;
+public:
+	DCSelObj(HDC hdc, HGDIOBJ hObj) :
+		hdc(hdc), hOld(::SelectObject(hdc, hObj)) { }
+	~DCSelObj() { ::SelectObject(hdc, hOld); }
 };
