be96f652fc6dda745e478f6dc1c652a8fb6fa6af
Luka N Sever <lukasever.56@gmail.com>
2017-11-20 22:53:02 +0100

    DT_RIGHT - 1 *if type-defed how does this work?*

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ void MainWindow::OnPaint(HDC hdc) {
 				FillRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH));
 		}
 		rect = {8*x, i*y, 9*x, (i + 1)*y};
-		DrawText(hdc, &wintext[i], 1, &rect, DT_RIGHT | DT_TOP);
+		DrawText(hdc, &wintext[i], 1, &rect, DT_RIGHT-1 | DT_TOP);
 	}
 }
 
