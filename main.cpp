29aae51175fa9604d89f70fc213212ab08512ee2
Mario Habijanec <mario.habijanec1@gmail.com>
2018-11-19 17:30:54 +0100

    pokušaj vježbe 6e

--- main.cpp
+++ main.cpp
@@ -6,27 +6,59 @@ int MyDialog::IDD(){
 }
 
 bool MyDialog::OnInitDialog(){
+	SetText(IDC_EDIT1, text);
 	return true;
 }
 
 bool MyDialog::OnOK(){
+	text = GetText(IDC_EDIT1);
 	return true;
 }
 
 
 void MainWindow::OnPaint(HDC hdc) {
+	RECT rc;
+	GetClientRect(*this, &rc);
+	long x = rc.right / 9;
+	long y = rc.bottom / window_text.size();
+	HFONT selected_font = (HFONT)SelectObject(hdc, CreateFontIndirect(&font_style));
+	for (int i = 0; i < window_text.size(); i++) {
+		for (int j = 0; j < 8; j++) {
+			rc = { j*x, i*y, (j + 1)*x, (i + 1)*y };
+			if ((window_text[i] & (1 << (7 - j))) == 0) FillRect(hdc, &rc, (HBRUSH)GetStockObject(BLACK_BRUSH));
+		}
+		rc = { 8 * x, i*y, 9 * x, (i + 1)*y };
+		DrawText(hdc, &window_text[i], 1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	}
+	DeleteObject((HFONT)SelectObject(hdc, selected_font));
+}
+
+void GetFont(HWND parent, LOGFONT &fontstyle) {
+	CHOOSEFONT sel_font;
+	ZeroMemory(&sel_font, sizeof(sel_font));
+	sel_font.hwndOwner = parent;
+	sel_font.lStructSize = sizeof(sel_font);
+	sel_font.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
+	sel_font.lpLogFont = &fontstyle;
+	ChooseFont(&sel_font);
 }
 
 void MainWindow::OnCommand(int id) {
 	switch(id){
 	case ID_FONT:
+		GetFont(*this, font_style);
 		break;
-	case ID_TEXT:
-		break;
+	case ID_TEXT: {
+		MyDialog dialog;
+		dialog.text = window_text;
+		if (dialog.DoModal(0, *this) == IDOK)
+			window_text = dialog.text;
+		break; }
 	case ID_EXIT:
 		DestroyWindow(*this);
 		break;
 	}
+	InvalidateRect(*this, NULL, true);
 }
 
 void MainWindow::OnDestroy(){
--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #include "nwpdlg.h"
 
 class MyDialog : public Dialog {
+public:
+	tstring text;
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -9,6 +11,15 @@ protected:
 };
 
 class MainWindow : public Window {
+public:
+	LOGFONT font_style;
+	tstring window_text;
+	MainWindow() : window_text("težak zadatak") {
+		ZeroMemory(&font_style, sizeof(font_style));
+		HDC hdc = GetDC(0);
+		font_style.lfHeight = -20 * GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+		ReleaseDC(0, hdc);
+	}
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
