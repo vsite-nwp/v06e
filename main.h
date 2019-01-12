0329b63f6c1d9f66e0c8d31257e449f4ffc37d21
GordanSentic <40005099+GoSen26@users.noreply.github.com>
2019-01-13 00:06:32 +0100

    finish

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
+
 #include "nwpwin.h"
 #include "nwpdlg.h"
 
 class MyDialog : public Dialog {
+public:
+	tstring text;
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -9,8 +12,18 @@ protected:
 };
 
 class MainWindow : public Window {
+	
+	tstring str;
+	LOGFONT lf;
+	COLORREF color;
+public:
+	MainWindow();
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
 };
+
+
+
+
