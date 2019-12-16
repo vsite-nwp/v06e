350c87586accc1587491cd7f4388e646754acf89
Ivan Mihin <ivmihin@vsite.hr>
2019-12-16 12:42:17 +0100

    gotova vježba 6e

--- main.cpp
+++ main.cpp
@@ -1,31 +1,85 @@
 #include "main.h"
 #include "rc.h"
 
+bool GetFont(HWND parent, LOGFONT& log, COLORREF& color)
+{
+	CHOOSEFONT cf;
+	ZeroMemory(&cf, sizeof cf);
+	cf.lStructSize = sizeof cf;
+	cf.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
+	cf.hwndOwner = parent;
+	cf.lpLogFont = &log;
+	cf.rgbColors = color;
+	bool fontChange = ChooseFont(&cf);
+	color = cf.rgbColors;
+	return fontChange;
+}
+
 int MyDialog::IDD(){
 	return IDD_DIALOG;
 }
 
 bool MyDialog::OnInitDialog(){
+	SetText(IDC_EDIT1, text);
 	return true;
 }
 
 bool MyDialog::OnOK(){
+	text = GetText(IDC_EDIT1);
 	return true;
 }
 
-
 void MainWindow::OnPaint(HDC hdc) {
+	if (windowText.size() == 0) {
+		return;
+	}
+	RECT screen;
+	GetClientRect(*this, &screen);
+	int x = screen.right / 9;
+	int y = screen.bottom / windowText.size();
+	HFONT previous_font = (HFONT)SelectObject(hdc, CreateFontIndirect(&log));
+	SetTextColor(hdc, color);
+	for (int i = 0; i < windowText.size(); ++i) {
+		for (int j = 0; j < 8; ++j) {
+			screen = { j*x, i*y, (j + 1)*x, (i + 1)*y };
+			if ((windowText[i] & (1 << (7 - j))) == 0) {
+				FillRect(hdc, &screen, (HBRUSH)GetStockObject(BLACK_BRUSH));
+			}
+		}
+		screen = { 8 * x , i*y, 9 * x , (i + 1)*y };
+		DrawText(hdc, &windowText[i], 1, &screen, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	}
+	DeleteObject((HFONT)SelectObject(hdc, previous_font));
 }
 
 void MainWindow::OnCommand(int id) {
-	switch(id){
-	case ID_FONT:
+	LOGFONT logCopy = log;
+	COLORREF colorCopy = color;
+
+	switch (id) {
+	case ID_FONT: {
+		if (GetFont(*this, log, color)) {
+			InvalidateRect(*this, NULL, true);
+		}
+		else {
+			log = logCopy;
+			color = colorCopy;
+		}
 		break;
-	case ID_TEXT:
+	}
+	case ID_TEXT: {
+		MyDialog dlg;
+		dlg.text = windowText;
+		if (dlg.DoModal(NULL, *this) == IDOK) {
+			windowText = dlg.text;
+			InvalidateRect(*this, NULL, true);
+		}
 		break;
-	case ID_EXIT:
+	}
+	case ID_EXIT: {
 		DestroyWindow(*this);
 		break;
+	}
 	}
 }
 
--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #include "nwpdlg.h"
 
 class MyDialog : public Dialog {
+public:
+	tstring text;
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -9,6 +11,18 @@ protected:
 };
 
 class MainWindow : public Window {
+public:
+	tstring windowText;
+	LOGFONT log;
+	COLORREF color;
+	MainWindow() {
+		::ZeroMemory(&log, sizeof(log));
+		HDC hdc = GetDC(0);
+		_tcscpy(log.lfFaceName, _T("Times New Roman"));
+		log.lfHeight = -18 * GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+		log.lfCharSet = EASTEUROPE_CHARSET;
+		color = RGB(0, 0, 0);
+	}
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
