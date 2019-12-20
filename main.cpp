70dcdb53f7886fa13c0b3ba1788c9df866f6df16
Bruno Čale <brcale@vsite.hr>
2019-12-20 19:16:28 +0100

    Prepravljeno

--- main.cpp
+++ main.cpp
@@ -5,20 +5,16 @@
 int MyDialog::IDD() {
 	return IDD_DIALOG;
 }
-
 bool MyDialog::OnInitDialog() {
 	SetText(IDC_EDIT1, tekst);
 	return true;
 }
-
 bool MyDialog::OnOK() {
 	tekst = GetText(IDC_EDIT1);
 	return true;
 }
-
-
 void MainWindow::OnPaint(HDC hdc) {
-	if (!str.size())
+	if (str.size()==0)
 		return;
 	RECT rec;
 	GetClientRect(*this, &rec);
@@ -27,21 +23,24 @@ void MainWindow::OnPaint(HDC hdc) {
 	SetTextColor(hdc, col);
 	HFONT hf = (HFONT)SelectObject(hdc, CreateFontIndirect(&logf));
 	HBRUSH hb = (HBRUSH)GetStockObject(BLACK_BRUSH);
-	for (int i = 0; i < str.size(); i++) {
-		for (int j = 0; j < 8; j++) {
+	int i = 0;
+	do {
+		int j = 0;
+		do {
 			rec = {
 				j * rig,i * bot,(j + 1) * rig,(i + 1) * bot
 			};
 			bool bijela = str[i] & (1 << (7 - j));
-			if (!bijela)
+			if (bijela == false)
 				FillRect(hdc, &rec, hb);
-		}
+			j++;
+		} while (j < 8);
 		rec = {
 			8 * rig,i * bot,9 * rig,(i + 1) * bot
 		};
 		DrawText(hdc, &str[i], 1, &rec, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-
-	}
+		i++;
+	} while (i < str.size());
 	DeleteObject(SelectObject(hdc, hf));
 }
 MainWindow::MainWindow() : col(RGB(0, 0, 0)) {
@@ -70,36 +69,25 @@ void MainWindow::OnCommand(int id) {
 	MyDialog dialog;
 	LOGFONT font = logf;
 	COLORREF color = col;
-	switch (id) {
-	case ID_FONT:
-	{
+	if (id == ID_FONT)
 		if (GetFont(*this, logf, color))
 			InvalidateRect(*this, NULL, true);
-		else { logf = font; col = color; }
-		break;
-	}
-	case ID_TEXT:
-	{
-		MyDialog dialog;
-		dialog.tekst = str;
-		if (dialog.DoModal(NULL, *this) == IDOK)
-			this->str = dialog.tekst;
-		InvalidateRect(*this, NULL, true);
-		break;
-	}
-	case ID_EXIT:
-	{
+		else {
+			logf = font; 
+			col = color;
+		}
+	if (id == ID_TEXT)
+	MyDialog dialog;
+	dialog.tekst = str;
+	if (dialog.DoModal(NULL, *this) == IDOK)
+		this->str = dialog.tekst;
+	InvalidateRect(*this, NULL, true);
+	if (id == ID_EXIT)
 		DestroyWindow(*this);
-		break;
-	}
-	}
 }
-
-
 void MainWindow::OnDestroy() {
 	::PostQuitMessage(0);
 }
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow) {
 	Application app;
 	MainWindow wnd;
