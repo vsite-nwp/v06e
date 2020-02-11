b3eb3c89ba1180ed76b4c69367242c1d7d9d8cd4
Auro Mazzi <auro.mazzi@gmail.com>
2020-02-12 00:22:36 +0100

    Riješeno.

--- main.cpp
+++ main.cpp
@@ -6,27 +6,68 @@ int MyDialog::IDD(){
 }
 
 bool MyDialog::OnInitDialog(){
+	SetText(IDC_EDIT1, text);
 	return true;
 }
 
 bool MyDialog::OnOK(){
+	text = GetText(IDC_EDIT1);
 	return true;
 }
 
+void GetFont(HWND parent, LOGFONT &lf, COLORREF& color)
+{
+	CHOOSEFONT cf;
+	ZeroMemory(&cf, sizeof cf);
+	cf.lStructSize = sizeof cf;
+	cf.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
+	cf.hwndOwner = parent;
+	cf.lpLogFont = &lf;
+	cf.rgbColors = color;
+	if (ChooseFont(&cf))
+		color = cf.rgbColors;
+}
 
 void MainWindow::OnPaint(HDC hdc) {
+	if (!str.size())
+		return;
+	RECT rc;
+	GetClientRect(*this, &rc);
+	int x = rc.right / 9;
+	int y = rc.bottom / str.size();
+	SetTextColor(hdc, color);
+	HFONT hFont = (HFONT)SelectObject(hdc, CreateFontIndirect(&lf));
+	for (int i = 0; i < str.size(); i++) {
+		for (int j = 0; j < 8; j++) {
+			rc = { j * x, i * y, (j + 1) * x, (i + 1) * y };
+			if ((str[i] & (1 << (7 - j))) == 0)
+				FillRect(hdc, &rc, (HBRUSH)GetStockObject(BLACK_BRUSH));
+		}
+		rc = { 8 * x, i * y, 9 * x, (i + 1) * y };
+		DrawText(hdc, &str[i], 1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	}
+	DeleteObject((HFONT)SelectObject(hdc, hFont));
 }
 
 void MainWindow::OnCommand(int id) {
 	switch(id){
 	case ID_FONT:
+		GetFont(*this, lf, color);
 		break;
 	case ID_TEXT:
+	{
+		MyDialog dialog;
+		dialog.text = str;
+		if (dialog.DoModal(0, *this) == IDOK) {
+			str = dialog.text;
+		}
 		break;
+	}
 	case ID_EXIT:
 		DestroyWindow(*this);
 		break;
 	}
+	InvalidateRect(*this, NULL, true);
 }
 
 void MainWindow::OnDestroy(){
--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #include "nwpdlg.h"
 
 class MyDialog : public Dialog {
+public:
+	tstring text;
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -9,6 +11,26 @@ protected:
 };
 
 class MainWindow : public Window {
+public:
+	tstring str;
+	LOGFONT lf;
+	COLORREF color;
+	MainWindow()
+	{
+		HDC hdc = GetDC(0);
+		lf.lfHeight = -18 * GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+		lf.lfStrikeOut = false;
+		lf.lfUnderline = false;
+		lf.lfItalic = false;
+		lf.lfCharSet = EASTEUROPE_CHARSET;
+		lf.lfWeight = 400;
+		lf.lfOrientation = 0;
+		lf.lfEscapement = 0;
+		_tcscpy(lf.lfFaceName, _T("Arial"));
+		color = RGB(0, 0, 0);
+		ReleaseDC(0, hdc);
+	}
+
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
