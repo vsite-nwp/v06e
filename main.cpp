59f94a8de1383e90d0eb3a87fe6e9245e18c611a
Zoran Medić <zomedic@hotmail.com>
2022-04-11 21:26:12 +0200

    pocetak neceg

--- main.cpp
+++ main.cpp
@@ -5,21 +5,43 @@ int main_dialog::idd() const {
 	return IDD_DIALOG;
 }
 bool main_dialog::on_init_dialog() {
+	set_text(IDC_EDIT1, text);
 	return true;
 }
 bool main_dialog::on_ok() {
+	text = get_text(IDC_EDIT1);
 	return true;
 }
 
 void main_window::on_paint(HDC hdc) {
+	if (!str.size())
+		return;
+	RECT rect;
+	GetClientRect(*this, &rect);
+	int x = rect.right / 9;
+	int y = rect.bottom / str.size(); 
+	for (int i = 0; i < str.size(); i++) {
+		for (int k = 0; k < 8;k++) {
+			rect = { x * k, y * i, x * (k + 1), y * (i + 1) };
+			if ((str[i] & (1 << k)) == 0) {
+				FillRect(hdc, &rect, (HBRUSH) BLACK_BRUSH);
+			}
+		}
+		
+	}
+	
 }
 
 void main_window::on_command(int id) {
 	switch(id){
 		case ID_FONT:
 			break;
-		case ID_TEXT:
+		case ID_TEXT: 
+		{
+			main_dialog t;
+			t.text = str;
 			break;
+		}
 		case ID_EXIT:
 			::DestroyWindow(*this);
 			break;
