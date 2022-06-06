5bcf3c0041cbf568aa8819f61ae53b257c2ecbe7
Tomislav Petrović <topetrov@vsite.hr>
2022-06-06 23:17:28 +0200

    Finished

--- main.cpp
+++ main.cpp
@@ -1,32 +1,104 @@
 #include "main.h"
 #include "rc.h"
 
+
+
 int main_dialog::idd() const {
 	return IDD_DIALOG;
 }
 bool main_dialog::on_init_dialog() {
+	set_text(IDC_EDIT1, txt);
 	return true;
 }
 bool main_dialog::on_ok() {
+	txt = get_text(IDC_EDIT1);
 	return true;
 }
 
+main_window::main_window() {
+	_tcscpy_s(lf.lfFaceName, _T("Arial"));
+	HDC hdc = ::GetDC(0);
+	ZeroMemory(&lf, sizeof(lf));
+	lf.lfHeight = -16 * ::GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+	cr = RGB(0, 0, 0);
+	::ReleaseDC(0, hdc);
+}
+
+bool get_font(HWND hw, LOGFONT& logf, COLORREF& cr) {
+	LOGFONT lf = { logf };
+	CHOOSEFONT cf;
+	ZeroMemory(&cf, sizeof cf);
+	cf.lStructSize = sizeof cf;
+	cf.Flags = CF_INITTOLOGFONTSTRUCT
+		| CF_SCREENFONTS | CF_EFFECTS;
+	cf.lpLogFont = &lf;
+	if (ChooseFont(&cf)) {
+		logf = lf;
+		CreateFontIndirect(&lf);
+		cr = cf.rgbColors;
+		return true;
+	}
+	return false;
+
+}
+
 void main_window::on_paint(HDC hdc) {
+	RECT r;
+	GetClientRect(*this, &r);
+
+	int x = r.right / 9;
+
+	if (txt.empty()) 
+	{
+		return;
+	}
+
+	int y = r.bottom / txt.size();
+
+	HFONT hf = (HFONT)SelectObject(hdc, CreateFontIndirect(&lf));
+	SetTextColor(hdc, cr);
+	HBRUSH hb = (HBRUSH)GetStockObject(BLACK_BRUSH);
+
+	for (int i = 0; i < txt.size(); i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			r = { j * x, i * y, (j + 1) * x, (i + 1) * y };
+
+			if ((txt[i] & (1 << (7 - j))) == 0)
+			{
+				FillRect(hdc, &r, hb);
+			}
+		}
+		r = { 8 * x, i * y, 9 * x, (i + 1) * y };
+		DrawText(hdc, &txt[i], 1, &r, DT_VCENTER | DT_CENTER | DT_SINGLELINE);
+	}
+	DeleteObject((HFONT)SelectObject(hdc, hf)); 
 }
 
 void main_window::on_command(int id) {
-	switch(id){
-		case ID_FONT:
-			break;
-		case ID_TEXT:
-			break;
-		case ID_EXIT:
-			::DestroyWindow(*this);
-			break;
+	switch (id) {
+	case ID_FONT:
+		get_font(*this, lf, cr);
+		break;
+	case ID_TEXT:
+	{
+		main_dialog t;
+		t.txt = txt;
+		if (t.do_modal(0, *this) == IDOK) {
+			txt = t.txt;
+		}
+		break;
 	}
+	case ID_EXIT:
+		::DestroyWindow(*this);
+		break;
+	}
+	InvalidateRect(*this, NULL, true);
 }
 
-void main_window::on_destroy(){
+
+void main_window::on_destroy() {
 	::PostQuitMessage(0);
 }
 
--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #include "nwpdlg.h"
 
 class main_dialog : public vsite::nwp::dialog {
+public:
+	tstring txt;
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
@@ -9,6 +11,12 @@ protected:
 };
 
 class main_window : public vsite::nwp::window {
+public:
+	tstring txt;
+	LOGFONT lf;
+	COLORREF cr;
+	main_window();
+
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
