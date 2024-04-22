a658523f4c13a0e85878b550127df8cdac194cda
Ivan Davidović <hedead421@gmail.com>
2024-04-22 21:05:41 +0200

    Vježba 6e

--- main.cpp
+++ main.cpp
@@ -5,20 +5,65 @@ int main_dialog::idd() const {
 	return IDD_DIALOG;
 }
 bool main_dialog::on_init_dialog() {
+	set_text(IDC_EDIT1, txt);
 	return true;
 }
 bool main_dialog::on_ok() {
+	txt = get_text(IDC_EDIT1);
 	return true;
 }
 
 void main_window::on_paint(HDC hdc) {
+	RECT r;
+	GetClientRect(*this, &r);
+	HFONT hf = CreateFontIndirect(&lf);
+	SelectObject(hdc, hf);
+
+	if (!str.size())
+		return;
+
+	int x = r.right / 9;
+	int y = r.bottom / str.size();
+	SetTextColor(hdc, color);
+
+	HBRUSH hb = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	for (int i = 0; i < str.length(); i++) {
+		for (int k = 0; k < 8; k++) {
+			const RECT r = { x * k, y * i, x * (k + 1), y * (i + 1) };
+			if ((str[i] & (1 << (7 - k))) == 0) {
+				FillRect(hdc, &r, hb);
+			}
+		}
+		RECT r = { 8 * x, i * y, 9 * x, (i + 1) * y };
+		::DrawText(hdc, &str[i], 1, &r, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	}
+	DeleteObject(SelectObject(hdc, hf));
 }
 
 void main_window::on_command(int id) {
-	switch(id){
-		case ID_FONT:
-			break;
-		case ID_TEXT:
+	switch (id) {
+	case ID_FONT: {
+
+
+		CHOOSEFONT cf;
+		ZeroMemory(&cf, sizeof cf);
+		cf.lStructSize = sizeof cf;
+		cf.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
+		cf.lpLogFont = &lf;
+		if (ChooseFont(&cf)) {
+			InvalidateRect(*this, 0, true);
+			color = cf.rgbColors;
+		}
+	}
+				break;
+	case ID_TEXT: {
+				main_dialog dlg;
+				dlg.txt = str;
+				if (dlg.do_modal(0, *this) == IDOK) {
+					this->str = dlg.txt;
+					InvalidateRect(*this, NULL, true);
+				}
+	}
 			break;
 		case ID_EXIT:
 			::DestroyWindow(*this);
--- main.h
+++ main.h
@@ -2,6 +2,9 @@
 #include "nwpdlg.h"
 
 class main_dialog : public vsite::nwp::dialog {
+public:
+	tstring txt;
+
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
@@ -9,6 +12,23 @@ protected:
 };
 
 class main_window : public vsite::nwp::window {
+	tstring str;
+	LOGFONT lf;
+	COLORREF color;
+public:
+	main_window() {
+		_tcscpy_s(lf.lfFaceName, _T("Arial"));
+		HDC hdc = ::GetDC(0);
+		lf.lfHeight = -16 * ::GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+		color = RGB(0, 0, 0);
+		lf.lfCharSet = EASTEUROPE_CHARSET;
+		lf.lfStrikeOut = false;
+		lf.lfUnderline = false;
+		lf.lfItalic = false;
+		lf.lfOrientation = 0;
+		lf.lfEscapement = 0;
+		::ReleaseDC(0, hdc);
+	}
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
